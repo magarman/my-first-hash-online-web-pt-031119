4e3ca3b9266a3be17a0ad53b1ecb8bba29946587
Mari Garman <garman.mariana@gmail.com>
2019-03-26 00:59:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,8 +4,8 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"Whale_bonecorsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
-  return the_manifest
+  the_manifest = {"whale bone corsets" => 5, 
+"porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
